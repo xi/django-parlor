153ab1a2ba24a762c06d6b6c9b5e4bf7170b6fdf
Tobias Bengfort <tobias.bengfort@posteo.de>
2025-10-20 16:39:11 +0200

    fix TranslatableAdmin for create forms
    obj is None in that case

--- parlor/admin.py
+++ parlor/admin.py
@@ -6,7 +6,7 @@ class TranslatableAdmin(admin.ModelAdmin):
 
     def get_inlines(self, request, obj):
         class TranslationInline(self.translation_inline_class):
-            model = obj.translations.model
+            model = self.model().translations.model
             min_num = 2
             extra = 0
         return [
